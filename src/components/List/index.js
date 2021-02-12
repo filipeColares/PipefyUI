0835d8ff0f1d78fa15df267283e9a99b49a727ba
Filipe <filipecolaresdesouza@gmail.com>
2021-02-12 14:37:19 -0300

    Criando front-end pipefy

---
+++ src/components/List/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Container} from './styles';
+import Card from '../Card/index';
+import { MdAdd } from 'react-icons/md';
+
+export default function List({ data, index: listIndex }){
+    return(
+        <Container done={data.done}> 
+            <header>
+                <h2>{data.title}</h2>
+                {data.creatable &&(
+                    <button type="button">
+                        <MdAdd size={24} color="#FFF" />
+                    </button>
+                )}
+            </header>
+            <ul>
+                {data.cards.map((card, index) => <Card key={card.id} listIndex={listIndex} index={index} data={card}/>)}
+            </ul>
+        </Container>
+    );
+}
